d526e5ca8afc9e0a2ecb19545371d7b12702b409
SirBlast <juandaquelal14@gmail.com>
2022-08-30 09:06:25 -0500

    Subiendo Proyecto

---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+
+
+Route::view('/','index');
+
+Route::get('dashboard', [App\Http\Controllers\PageController::class,'dashboard'])
+->middleware('auth:sanctum')
+->name('dashboard');
+
+Route::resource('notes',App\Http\Controllers\NoteController::class)
+->middleware('auth:sanctum');
